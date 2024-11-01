6dd5a7795d0dfc0b918d9a9a365d502abc23d680
Sangamesh <132161994+sangamesh-Lingshetty@users.noreply.github.com>
2024-11-01 21:22:53 +0530

    Update Login.js

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -30,7 +30,7 @@ const Login = ({onSuccess}) => {
     }
     
     try{
-        const url = "https://socio-ivory.vercel.app/user/login";
+        const url = "https://socio-omega.vercel.app/user/login";
         const response = await fetch(url,{
             method:'POST',
             headers:{
